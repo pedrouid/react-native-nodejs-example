dc88a5b3f7df55ecf631305c1a001224eb1326a3
Pedro Gomes <pedrogomes94@gmail.com>
2019-01-28 20:22:04 +0000

    link nodejs-mobile-react-native

--- App.js
+++ App.js
@@ -1,7 +1,24 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import nodejs from "nodejs-mobile-react-native";
 
 export default class App extends React.Component {
+  componentDidMount() {
+    this.test();
+  }
+
+  test = () => {
+    nodejs.start("main.js");
+    nodejs.channel.send("ping");
+    nodejs.channel.addListener(
+      "message",
+      msg => {
+        alert("NodeJS Message => " + msg);
+      },
+      this
+    );
+  };
+
   render() {
     return (
       <View style={styles.container}>
@@ -14,8 +31,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
--- nodejs-assets/nodejs-project/main.js
+++ nodejs-assets/nodejs-project/main.js
@@ -1,7 +1,9 @@
 const RNBridge = require("rn-bridge");
 
-RNBridge.channel.on("message", msg => {
-  RNBridge.channel.send(msg);
-});
-
 RNBridge.channel.send("Node was initialized.");
+
+RNBridge.channel.on("message", msg => {
+  if (msg === "ping") {
+    RNBridge.channel.send("pong");
+  }
+});
