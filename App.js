9ebf744b3f58634d3f2044855ea37d14874838a8
Pedro Gomes <pedrogomes94@gmail.com>
2019-01-28 20:47:37 +0000

    install ipfs

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Platform, StyleSheet, Text, View } from "react-native";
 import nodejs from "nodejs-mobile-react-native";
+import RNFS from "react-native-fs";
+
+const DOCUMENT_DIRECTORY =
+  Platform.OS === "ios" ? RNFS.MainBundlePath : RNFS.DocumentDirectoryPath;
 
 export default class App extends React.Component {
   componentDidMount() {
@@ -9,11 +13,19 @@ export default class App extends React.Component {
 
   test = () => {
     nodejs.start("main.js");
-    nodejs.channel.send("ping");
+    nodejs.channel.send(
+      JSON.stringify({
+        action: "init",
+        data: { docsPath: DOCUMENT_DIRECTORY }
+      })
+    );
     nodejs.channel.addListener(
       "message",
       msg => {
-        alert("NodeJS Message => " + msg);
+        const payload = JSON.parse(msg);
+        if (payload.action === "message") {
+          alert("Message: " + payload.data.message);
+        }
       },
       this
     );
--- nodejs-assets/nodejs-project/main.js
+++ nodejs-assets/nodejs-project/main.js
@@ -1,9 +1,62 @@
 const RNBridge = require("rn-bridge");
+const IPFS = require("ipfs");
 
-RNBridge.channel.send("Node was initialized.");
+let ipfs = null;
+
+function initIpfs(docsPath) {
+  const ipfsConfig = {
+    init: {
+      bits: 1024,
+      emptyRepo: true,
+      log: sendState
+    },
+    repo: path.resolve(docsPath, "./testing"),
+    EXPERIMENTAL: {
+      dht: false,
+      relay: {
+        enabled: true,
+        hop: {
+          enabled: false,
+          active: false
+        }
+      },
+      pubsub: true
+    },
+    config: {
+      Bootstrap: [],
+      Addresses: {
+        Swarm: ["/ip4/159.203.117.254/tcp/9090/ws/p2p-websocket-star"]
+      }
+    },
+    connectionManager: {
+      maxPeers: 10,
+      minPeers: 2,
+      pollInterval: 20000
+    }
+  };
+
+  ipfs = new IPFS(ipfsConfig);
+
+  RNBridge.channel.send(
+    JSON.stringify({
+      action: "message",
+      data: { message: "IPFS Initialized" }
+    })
+  );
+}
 
 RNBridge.channel.on("message", msg => {
-  if (msg === "ping") {
-    RNBridge.channel.send("pong");
+  const payload = JSON.parse(msg);
+  if (payload.action === "init") {
+    initIpfs(payload.data.docsPath);
   }
+});
+
+ipfs.on("ready", () => {
+  RNBridge.channel.send(
+    JSON.stringify({
+      action: "message",
+      data: { message: "IPFS Ready" }
+    })
+  );
 });
