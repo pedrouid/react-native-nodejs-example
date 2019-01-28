7eb17dce515be9d0ab989d7fdb0f119e867f2370
Pedro Gomes <pedrogomes94@gmail.com>
2019-01-28 22:18:08 +0000

    patch

--- nodejs-assets/build-native-modules-MacOS-helper-script-node.sh
+++ nodejs-assets/build-native-modules-MacOS-helper-script-node.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
       # Helper script for Gradle to call node on macOS in case it is not found
-      export PATH=$PATH:/usr/local/lib/node_modules/npm/node_modules/npm-lifecycle/node-gyp-bin:/Users/pedrogomes/_sandbox/react-native-nodejs-example/node_modules/nodejs-mobile-react-native/node_modules/.bin:/Users/pedrogomes/_sandbox/react-native-nodejs-example/node_modules/.bin:/Users/pedrogomes/.cargo/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/go/bin:/usr/local/MacGPG2/bin:/Applications/Postgres.app/Contents/Versions/latest/bin
+      export PATH=$PATH:/usr/local/lib/node_modules/npm/node_modules/npm-lifecycle/node-gyp-bin:/Users/pedrogomes/_sandbox/react-native-nodejs-example/node_modules/nodejs-mobile-react-native/node_modules/.bin:/Users/pedrogomes/_sandbox/react-native-nodejs-example/node_modules/.bin:/usr/local/lib/node_modules/npm/node_modules/npm-lifecycle/node-gyp-bin:/Users/pedrogomes/_sandbox/react-native-nodejs-example/node_modules/.bin:/Users/pedrogomes/.cargo/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/go/bin:/usr/local/MacGPG2/bin:/Applications/Postgres.app/Contents/Versions/latest/bin:/Users/pedrogomes/.cargo/bin
       node $@
     
